7043a5f6f825e7c6c044c560c19a0fa6aa0fb2e9
AmanKamani <kamaniamanak@gmail.com>
2025-09-21 12:45:58 +0530

    Initial common-lib setup

---
+++ common-lib/src/main/java/com/shoppingplatform/commonlib/CommonLibApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingplatform.commonlib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommonLibApplication.class, args);
+    }
+
+}
